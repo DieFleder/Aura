38978855070cd97b54647e3d9c0f48675891a2e6
Matt McCaw <the_laughing_bandit@hotmail.com>
2023-12-18 13:34:08 -0800

    2-6  Player and Enemy Characters

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,17 @@
+// Copyright Emergent Engineering LLC
+
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
