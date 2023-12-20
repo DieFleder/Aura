7ed2726524ffcf48ad37a5bf63ec3f877d22a731
Matt McCaw <the_laughing_bandit@hotmail.com>
2023-12-19 21:34:24 -0800

    2-15  Highlight enemies on mouseover. Section 2 complete!

---
+++ Source/Aura/Private/Interaction/EnemyInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright Emergent Engineering LLC
+
+
+#include "Interaction/EnemyInterface.h"
+
+// Add default functionality here for any IEnemyInterface functions that are not pure virtual.
--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,10 +4,76 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if(!CursorHit.bBlockingHit){return;}
+
+	LastActor = CurrentActor;
+	CurrentActor = Cast<IEnemyInterface>(CursorHit.GetActor());
+	
+	/**
+	 *Line trace from cursor. Several Scenarios:
+	 *A. LastActor is null && CurrentActor is null
+	 *	- Do nothing.
+	 *B. LastActor is null && CurrentActor is valid
+	 *	- Highlight CurrentActor
+	 *C. LastActor is Valid && CurrentActor is null
+	 *	- UnHighlight LastActor
+	 *D. Both are valid but LastActor != CurrentActor
+	 *	- UnHighlight LastActor and Highlight CurrentActor
+	 *E. Both are valid and LastActor == CurrentActor
+	 *	- do nothing
+	 **/
+	if(LastActor == nullptr)
+	{
+		if(CurrentActor != nullptr)
+		{
+			// Case B.
+			CurrentActor->HighlightActor();
+		}
+		else
+		{
+			//Case A. Do nothing
+		}
+	}
+	else // LastActor is valid
+	{
+		if(CurrentActor == nullptr)
+		{
+			// Case C
+			LastActor->UnHighlightActor();
+		}
+		else //Both are valid
+		{
+			if(LastActor != CurrentActor)
+			{
+				// Case D.
+				LastActor->UnHighlightActor();
+				CurrentActor->HighlightActor();
+			}
+			else // both are valid but not the same actor
+			{
+				// Case E. do nothing
+			}
+		}
+	}
+	
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -52,3 +118,4 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
 	}
 }
+
